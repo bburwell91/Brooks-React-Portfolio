b8170afdf9d409bc71da3d24176772db415d6caa
Brooks Burwell <brooksdburwell@gmail.com>
2019-10-17 17:23:15 -0700

    Updates

---
+++ precache-manifest.038525e41edec08f1d5b4dfbfb990568.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562893cb3450670dae22df7159fa1267",
+    "url": "/Brooks-React-Portfolio/index.html"
+  },
+  {
+    "revision": "959d6ccd25dfd4b3aed8",
+    "url": "/Brooks-React-Portfolio/static/css/main.35893909.chunk.css"
+  },
+  {
+    "revision": "fe62841707ac72c838a5",
+    "url": "/Brooks-React-Portfolio/static/js/2.6013096d.chunk.js"
+  },
+  {
+    "revision": "959d6ccd25dfd4b3aed8",
+    "url": "/Brooks-React-Portfolio/static/js/main.17348383.chunk.js"
+  },
+  {
+    "revision": "88dcee1d19f5de3b7039",
+    "url": "/Brooks-React-Portfolio/static/js/runtime-main.2689c76b.js"
+  }
+]);
